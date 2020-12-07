d6a4909d520fd20002c13b85a77153672c640843
Alexandros <aaidonis1932@bths.edu>
2020-12-07 13:20:44 -0500

    Added Comments

--- caesar.cpp
+++ caesar.cpp
@@ -2,8 +2,9 @@
 #include "caesar.h"
 
 char shiftChar(char c, int rshift){
-   int num;
-   if(isupper(c)){
+   int num; // Writing comments for the dev branch 
+   if(isupper(c)){ // More comments 
+   // Added a space 
       num = ((int)c - 65 + rshift) % 26 + 65; 
       if(num<65){
          return char(26+num);
@@ -13,11 +14,12 @@ char shiftChar(char c, int rshift){
    else if(islower(c)){
       num = ((int)c - 96 + rshift) % 26 + 96;
       if(num < 97){
-        return char(26+num); 
+        return char(26+num); // Comments 
       } 
       return char(num);
    }
    return c;
+   // Added another space
 }
 
 std::string encryptCaesar(std::string plaintext, int rshift){
